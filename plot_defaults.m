a1e12013a64bd37541fd0e3527cf7c731e595a53
Henry Ando <henry.ando@gmail.com>
2023-11-10 16:12:57 -0600

    Figure improvements

--- plot_defaults.m
+++ plot_defaults.m
@@ -6,3 +6,9 @@ set(groot, 'defaultScatterLineWidth', 1);
 set(groot, 'defaultAxesLineWidth', 1);
 set(groot, 'defaultAxesBox', true);
 set(groot, 'defaultAxesFontSize', 12);
+
+cs_cbar = cubehelix(256, -0.25, 0, 2, 1.1, [0,0.95], [0,0.95]);
+li_cbar = cubehelix(256, 1, 0, 2, 1.1, [0,0.95], [0,0.95]);
+
+cs_blue = "#0072BD";
+li_red = "#A2142F";
--- smart_fig_export.m
+++ smart_fig_export.m
@@ -1,6 +1,7 @@
 function smart_fig_export(fig, name)
 
 
-export_fig([name '.pdf'], fig);
+% export_fig([name '.pdf'], fig, "-transparent");
+exportgraphics(fig, [name '.pdf'], 'BackgroundColor','none','ContentType','vector')
 savefig(fig, [name '.fig']);
 
