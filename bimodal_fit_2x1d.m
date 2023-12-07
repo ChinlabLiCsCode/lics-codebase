5271257cf0cbf85a6e6eccc45bde2efeda9749a0
LiCs Analysis <chinlablics@gmail.com>
2023-12-07 15:18:11 -0600

    rework fermi_fit_2d

--- bimodal_fit_2x1d.m
+++ bimodal_fit_2x1d.m
@@ -2,18 +2,18 @@ function results = bimodal_fit_2x1d(params, img, varargin)
 
 % initialize default inputs
 crop = 'mask';
-c0x = 'guess';
-c0y = 'guess';
+% c0x = 'guess';
+% c0y = 'guess';
 
 % process varargin
 for setting = 1:2:length(varargin)
     switch varargin{setting}
         case 'crop'
             crop = varargin{setting + 1};
-        case 'c0x'
-            c0x = varargin{setting + 1};
-        case 'c0y'
-            c0y = varargin{setting + 1};
+        % case 'c0x'
+        %     c0x = varargin{setting + 1};
+        % case 'c0y'
+        %     c0y = varargin{setting + 1};
     end
 end
 
@@ -23,7 +23,7 @@ while ndims(img) > 2
     img = squeeze(mean(img, 1));
 end
 
-pix = params.pixel;
+
 
 % define crop window properly
 if ~isnumeric(crop)
@@ -41,36 +41,66 @@ end
 img = img(crop(3):crop(4), crop(1):crop(2));
 [Y, X] = size(img);
 
-xtrc = trapz(img, 1)';
-ytrc = trapz(img, 2);
-xpix = (1:X)' - X/2;
-ypix = (1:Y)' - Y/2;
+% rescale and make trace axes 
+pix = 1e6*params.pixel;
+xtrc = trapz(img, 1)' ./ pix; % units of atoms/um
+ytrc = trapz(img, 2) ./ pix; % units of atoms/um
+xum = pix .* (1:X)'; % units of um
+yum = pix .* (1:Y)'; % units of um
 
-disp(size(xtrc))
-disp(size(xpix));
-% define fit type
+% subtract off center of mass 
+xcom = sum(xtrc .* xum) ./ sum(xtrc)
+ycom = sum(ytrc .* yum) ./ sum(ytrc);
+xum = xum - xcom;
+yum = yum - ycom;
+
+% make finer xaxis for fits 
+xum_fine = linspace(min(xum), max(xum), 1000);
+yum_fine = linspace(min(yum), max(yum), 1000);
+
+
+% define fit types
 ftbm = fittype(@(x0, aTF, aG, rTF, rG, x)...
     fit1dbm(x0, aTF, aG, rTF, rG, x));
+fttf = fittype(@(x0, aTF, rTF, x)...
+    fit1dtf(x0, aTF, rTF, x));
 
-%% X fit 
+%% X fit
+
+% do a pure thomas-fermi fit first
+sp = [0, max(xtrc), std(xum)/4];
+lb = [-100, 1, 0.5];
+ub = [100, 2*max(xtrc), 300];
+
+tffitx = fit(xum, xtrc, fttf, Start=sp, Lower=lb, Upper=ub);
+tffvalsx = coeffvalues(tffitx); % values
+ul = confint(tffitx) % 95% confidence intervals 
+tffsex = (ul(2, :) - ul(1, :))/4; % 1 sigma error
+
 % set upper bounds, lower bounds, and starting point for fit params
-c0x = [0, max(xtrc), 0.1, 50, 100];
-lbx = [-100, 0, 0, 0, 0];
+sp = [0, 0.9*max(xtrc), 0.1*max(xtrc), std(xum)/4, std(xum)];
+lbx = [-100, 1, 1, 0.5, 0.5];
 ubx = [100, Inf, Inf, 300, 300];
 
 % do fit
-fox = fit(xpix, xtrc, ftbm, Start=c0x, Lower=lbx, Upper=ubx);
-
-% get traces 
-xtrace_bm = fox(xpix);
-xtrace_tf = fit1dtf(fox.x0, fox.aTF, fox.rTF, xpix);
-xtrace_gauss = fit1dgauss(fox.x0, fox.aG, fox.rG, xpix);
-
+fox = fit(xum, xtrc, ftbm, Start=sp, Lower=lbx, Upper=ubx);
 
 % get out useful parameters and error bounds from those fits 
-fvx = coeffvalues(fox); % values
-ul = confint(fox); % 95% confidence intervals 
+fvx = coeffvalues(fox) % values
+ul = confint(fox) % 95% confidence intervals 
 fvx_se = (ul(2, :) - ul(1, :))/4; % 1 sigma error
+disp(fvx_se);
+
+% if gaussian part is impossible to fit, do just a tf fit
+
+
+% get traces 
+xtrc_bm = fox(xum_fine);
+xtrc_tf = fit1dtf(fox.x0, fox.aTF, fox.rTF, xum_fine);
+xtrc_gauss = fit1dgauss(fox.x0, fox.aG, fox.rG, xum_fine);
+
+
+
       
 % get condensate number
 n0x = fox.aTF * fox.rTF * 16/15;
@@ -78,16 +108,22 @@ n0x = fox.aTF * fox.rTF * 16/15;
 n0x_se = n0x * sqrt(...
     (fvx_se(2)/fvx(2))^2 + (fvx_se(4)/fvx(4))^2);
 
-
-
-figure(Units='normalized', Position=[0.25, 0.25, 0.5, 0.5]);
+% plot result
+figure(Units='normalized', Position=[0.25, 0.25, 0.7, 0.5]);
+plot_defaults;
 tiledlayout(1, 2);
 
 nexttile;
 hold on;
-plot(xpix, xtrc, 'o');
-
-
+plot(xum_fine, xtrc_bm, '-', Color='k', LineWidth=1.5);
+plot(xum_fine, xtrc_tf, '--', Color=green);
+plot(xum_fine, xtrc_gauss, '--', Color=purple);
+scatter(xum, xtrc, 64, 'o', MarkerEdgeColor=blue, ...
+    MarkerFaceColor=blue, MarkerFaceAlpha=0.3);
+xlabel('Position [µm]');
+ylabel('Density [atoms/µm]');
+legend({'bimodal fit', 'Thomas-Fermi part', 'gaussian part', 'data'});
+xlim([min(xum), max(xum)]);
 
 results = struct('n0x', n0x, 'n0x_se', n0x_se, 'fox', fox);
 
--- fermi_fit_2d.m
+++ fermi_fit_2d.m
@@ -1,112 +1,104 @@
-function [fitvals, fiterrs, ToverTF, N, Nc] = fermi_fit_2d(dat, guess, params, fignum, convert_units)
-    % performs 2d fit of non-interacting fermions according to 
-    % [Ketterle, Zwierlein (2008) - Making, probing, understanding ultracold
-    % Fermi gases], eq. 65 and following
-    % inputs: 
-    %   dat: 2d array dat (e.g. average results of df_view_image_fk())
-    %   guess: amplitude, q, x0, Rx, y0, Ry, background
-    %        lengths in [mu m] if convert_units (default) else in [pix]
-    %   params: normally li_params_fk, needs .pixel and .wavelength
-    %   fignum: number of figure to plot, also determines name for saving
-    %   convert_units: lengths are [mu m] if 1 (default), else [pix]
-    % returns:
-    %   fitvals, fiterrs: same as 'guess', fiterrs is std error of fit
-    %       returns lengths in micro meters if convert_units == 1
-    %   ToverTF: temperature in units of Fermi temperature
-    %   N: total number of imaged atoms
-    
-    [L, H] = size(dat);
-    x_Li = (1:L); % 1d grids in [pix]
-    y_Li = (1:H); 
-    
-    px = params.pixel*1e6;  % conversion from pix to microns
-    clims = [min(min(dat)), max(max(dat))];  % limits for color scales
-    
-    if nargin < 5
-        convert_units = 1;
-    end
-    if convert_units
-        guess([3 4 5 6]) = guess([3 4 5 6])/px;
-    end
-    
-    figure(fignum);
-    subplot(3,1,1);
-    imagesc(x_Li*px, y_Li*px, dat', clims);
-    title("data"); ylabel("y / \mum");
-    colorbar();
+function [fitvals, fiterrs, ToverTF, N, Nc] = fermi_fit_2d(img, params, varargin)
+% performs 2d fit of non-interacting fermions according to 
+% [Ketterle, Zwierlein (2008) - Making, probing, understanding ultracold
+% Fermi gases], eq. 65 and following
+% inputs: 
+%   dat: 2d array dat (e.g. average results of df_view_image_fk())
+%   guess: amplitude, q, x0, Rx, y0, Ry, background
+%        lengths in [mu m] if convert_units (default) else in [pix]
+%   params: normally li_params_fk, needs .pixel and .wavelength
+%   convert_units: lengths are [mu m] if 1 (default), else [pix]
+% returns:
+%   fitvals, fiterrs: same as 'guess', fiterrs is std error of fit
+%       returns lengths in micro meters if convert_units == 1
+%   ToverTF: temperature in units of Fermi temperature
+%   N: total number of imaged atoms
 
-    % set up 2d grid in [pix]
-    [Xin, Yin] = meshgrid(x_Li, y_Li);  
-    XY(:, :, 1) = Xin;
-    XY(:, :, 2) = Yin;
+while ndims(img) > 2
+    img = squeeze(mean(img, 1));
+end
+[nY, nX] = size(img);
+yvals = (1:nY)'; % 1d grids in [pix]
+xvals = (1:nX)'; 
 
-    subplot(3,1,2);
+pix = params.pixel*1e6;  % conversion from pix to microns
+xvals = xvals;
+yvals = yvals;
+img = img;
 
-    lb = [0 -Inf 0 0 0 0 -Inf]; % z > -.9 for fermi_fit
-    ub = [Inf 9 Inf Inf Inf Inf Inf];  % z < 3.5 for fermi_fit
-    [fitvals,~,res,~,~,~,jacob] = lsqcurvefit(@fermi_fit_Ketterle, guess, XY, dat', lb, ub);
-    fermi_fit = fermi_fit_Ketterle(fitvals, XY);
-    fermi_fit = abs(fermi_fit);
-    imagesc(x_Li*px, y_Li*px, fermi_fit, clims);
-    fiterrs = nlstandarderror(jacob,res);
-    colorbar();
-    
-    ToTF = get_ToverTF(exp(fitvals(2)));
-    u = get_ToverTF(exp(fitvals(2) + fiterrs(2))) - ToTF;
-    l = get_ToverTF(exp(fitvals(2) - fiterrs(2))) - ToTF;
-    e = max(abs([l, u]));
-    
-    title(sprintf("fit, T/T_F = %.4f \\pm %.4f", ToTF, e)); 
-    ylabel("y / \mum");
+clims = [min(min(img)), max(max(img))];  % limits for color scales
 
-    subplot(3,1,3);
-    % imagesc(x_Li, y_Li, li_V_scan04imgsavgc' - fermi_fit_Ketterle(fitvals, XY));
-    % colorbar();
-    % title("data - fit");
-    plot(x_Li*px, sum(dat,2));
-    hold on;
-    plot(x_Li*px, sum(fermi_fit_Ketterle(fitvals, XY),1));
-    xlabel("x / \mum");
-    ylabel("col dens (a.u.)");
-    hold off
-    ylim([min(sum(dat,2)), max(sum(dat,2))]);
-    
-    savefig(sprintf("fermi_2dfit_%d", fignum));
-    saveas(gcf, sprintf("fermi_2dfit_%d.png", fignum));
+figure();
+subplot(3,1,1);
+imagesc(xvals, yvals, img, clims);
+title("data"); 
+ylabel("y [pix]");
+colorbar();
 
-    if nargout > 2
-        ToverTF = ToTF;
-        if nargout > 3
-            N = get_N(fitvals, params); % fitvals in [pix]!!
-            if nargout > 4
-                Nc = count_pixels(dat, params);
-            end
+% set up 2d grid in [pix]
+[Xgrid, Ygrid] = meshgrid(xvals, yvals);  
+
+subplot(3,1,2);
+
+% prep fit parameters
+ft = fittype(@(amp, q, x0, Rx, y0, Ry, bg, X, Y) ...
+    fermi_fit_Ketterle(amp, q, x0, Rx, y0, Ry, bg, X, Y),...
+    independent=["X", "Y"]);
+sp = [max(img(:)), 6, mean(xvals), mean(xvals), mean(yvals), mean(yvals), 0];
+lb = [0 -Inf 0 0 0 0 -Inf]; % z > -.9 for fermi_fit
+ub = [Inf 9 Inf Inf Inf Inf Inf];  % z < 3.5 for fermi_fit
+
+% do fit
+fo = fit([Xgrid(:), Ygrid(:)], img(:), ft, ...
+    Start=sp, Lower=lb, Upper=ub);
+% evaluate fit
+fermi_fit = reshape(fo([Xgrid(:), Ygrid(:)]), size(img));
+fermi_fit = abs(fermi_fit);
+
+% extract quantities 
+fitvals = coeffvalues(fo);
+fiterrs = confint(fo);
+fiterrs = (fiterrs(2, :) - fiterrs(1, :)) / 4;
+ToTF = get_ToverTF(exp(fitvals(2)));
+u = get_ToverTF(exp(fitvals(2) + fiterrs(2))) - ToTF;
+l = get_ToverTF(exp(fitvals(2) - fiterrs(2))) - ToTF;
+e = max(abs([l, u]));
+
+% plot fit
+imagesc(xvals, yvals, fermi_fit, clims);
+colorbar();
+title(sprintf("fit, T/T_F = %.4f \\pm %.4f", ToTF, e)); 
+ylabel("y [pix]");
+
+subplot(3,1,3);
+plot(xvals, sum(img, 1));
+hold on;
+plot(xvals, sum(fermi_fit, 1));
+xlabel("x [pix]");
+ylabel("density [atoms/pix]");
+hold off
+% ylim([min(sum(img,2)), max(sum(img,2))]);
+
+savefig(sprintf("fermi_2dfit"));
+saveas(gcf, sprintf("fermi_2dfit.png"));
+
+if nargout > 2
+    ToverTF = ToTF;
+    if nargout > 3
+        N = get_N(fitvals, params); % fitvals in [pix]!!
+        if nargout > 4
+            Nc = count_pixels(img, params);
         end
-    end
-    
-
-    if convert_units
-        % convert radii and center positions to microns
-        fitvals([3 4 5 6]) = fitvals([3 4 5 6])*px;
-        fiterrs([3 4 5 6]) = fiterrs([3 4 5 6])*px^2;
     end
 end
 
-function y = fermi_fit_Ketterle(c, XYin)
-    X = XYin(:, :, 1);   % meshgrid
-    Y = XYin(:, :, 2);
-    
-    amplitude = c(1);
-    q = c(2);  % q = beta mu, log of fugacity
-    x0 = c(3);
-    Rx = c(4);
-    y0 = c(5); 
-    Ry = c(6);
-    bg = c(7);
-    
+
+end
+
+function y = fermi_fit_Ketterle(amp, q, x0, Rx, y0, Ry, bg, X, Y)
     arg = exp(q -((X-x0).^2 ./ Rx^2 + (Y-y0).^2./Ry^2) .* f(exp(q)));
     % fprintf("min %.2f, max %.2f\n", min(arg(:)), max(arg(:)));
-    y = amplitude .* polylog_num(2, -arg) ./ polylog(2, -exp(q)) + bg;
+    y = amp .* polylog_num(2, -arg) ./ polylog(2, -exp(q)) + bg;
     
 end
 
@@ -125,7 +117,8 @@ function N = get_N(fitres, params)
     Ry = fitres(6); % in [pix]
     bg = fitres(7);
 
-    A = params.pixel^2/(3*params.wavelength^2/(2*pi)); % (pixel area)/sigma0
+    % A = params.pixel^2/(3*params.wavelength^2/(2*pi)); % (pixel area)/sigma0
+    A = 1; % pretty sure this factor is already accounted for now.
     temp = polylog(3, -exp(q))/polylog(2, -exp(q))*polylog(0, -exp(q))/polylog(1, -exp(q));
     N = A * pi * (n0-bg) * Rx * Ry * temp;
         
@@ -133,6 +126,7 @@ end
 
 
 function N = count_pixels(data, params)
-    A = params.pixel^2/(3*params.wavelength^2/(2*pi));
+    % A = params.pixel^2/(3*params.wavelength^2/(2*pi));
+    A = 1;
     N = sum(sum(data)) * A;
 end
