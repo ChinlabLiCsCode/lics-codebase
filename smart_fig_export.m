23bca3a3e1ba65dfe530078479bfbc4ba414ad5a
LiCs Analysis <chinlablics@gmail.com>
2024-03-07 16:58:58 -0600

    add png

--- smart_fig_export.m
+++ smart_fig_export.m
@@ -2,6 +2,6 @@ function smart_fig_export(fig, name)
 
 
 % export_fig([name '.pdf'], fig, "-transparent");
-exportgraphics(fig, [name '.pdf'], 'BackgroundColor','none','ContentType','vector')
-savefig(fig, [name '.fig']);
-
+exportgraphics(fig, strcat(name, '.pdf'), 'BackgroundColor','none','ContentType','vector')
+savefig(fig, strcat(name, '.fig'));
+saveas(fig, strcat(name, '.png')
