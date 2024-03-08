9c627d1abe56bd9146efee149d44d8d5713fabab
chinlablicsexp <chinlablics@gmail.com>
2024-03-07 18:01:01 -0600

    Fix bug in fig_export

--- smart_fig_export.m
+++ smart_fig_export.m
@@ -4,4 +4,4 @@ function smart_fig_export(fig, name)
 % export_fig([name '.pdf'], fig, "-transparent");
 exportgraphics(fig, strcat(name, '.pdf'), 'BackgroundColor','none','ContentType','vector')
 savefig(fig, strcat(name, '.fig'));
-saveas(fig, strcat(name, '.png')
+saveas(fig, strcat(name, '.png'));
