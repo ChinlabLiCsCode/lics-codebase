84a70755b5f99be97ce3098d2a301ab6f8dd3555
lics <chinlablics@gmail.com>
2025-08-20 16:17:36 -0500

    bug fixes

--- experiment.py
+++ experiment.py
@@ -1,9 +1,10 @@
-from labscript import start, stop, add_time_marker, Trigger, DigitalOut, load_globals
+from labscript import start, stop,add_time_marker, DigitalOut, AnalogOut
 from labscript.remote import RemoteBLACS
 from labscript_devices.DummyPseudoclock.labscript_devices import DummyPseudoclock
 from labscript_devices.NI_DAQmx.models.NI_PXIe_6536 import NI_PXIe_6536
+from labscript_devices.NI_DAQmx.models.NI_PXIe_6738 import NI_PXIe_6738
 
-from labscriptlib.example_apparatus4.connection_table import ConnectionTable
+from labscriptlib.LiCs_ExperimentApparatus.connection_table import ConnectionTable
 
 if __name__ == '__main__':
     ct = ConnectionTable()
@@ -15,13 +16,13 @@ if __name__ == '__main__':
     add_time_marker(t, "Start", verbose=True)
     start()
 
-    for i in range (1, int(31e4)):
+    for i in range (1, int(31e3)):
         t=2*i *1e-5
-        for out in ct.digOuts:
-            ct.digOuts[out].go_high(t=t)
+        for out in ct.DC2Outs:
+            ct.DC2Outs[out].go_high(t=t)
         t += 1 *1e-5
 
-        for out in ct.digOuts:
-            ct.digOuts[out].go_low(t=t)
+        for out in ct.DC2Outs:
+            ct.DC2Outs[out].go_low(t=t)
 
     stop(t)
