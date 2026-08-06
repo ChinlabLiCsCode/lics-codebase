c511c94d86e3c16f77ecad84c74bab4d3878a479
chinlablicsexp <chinlablics@gmail.com>
2026-08-06 18:29:15 -0500

    updated cs subseqeunces to clean up timing and to implement global variables, updated mot healthcheck to do mot loading, optical molasses, and then lfhimg

--- lics_labscript_apparatus/sequences/cs_mot_healthcheck.py
+++ lics_labscript_apparatus/sequences/cs_mot_healthcheck.py
@@ -1,6 +1,6 @@
 from labscript import start, stop, add_time_marker, wait
 from lics_labscript_apparatus.connection_table import ConnectionTable
-from lics_labscript_apparatus.sequences.cs_subsequences import Cs_MOT_Loading, Cs_CMOT, Cs_Molasses_Cooling, Cs_H_Imaging
+from lics_labscript_apparatus.sequences.cs_subsequences import *
 
 if __name__ == '__main__':
     ct = ConnectionTable()
@@ -9,27 +9,41 @@ if __name__ == '__main__':
 
     t = 10e-6
 
+    t = ct.digital_jiggle(t)
     ct.set_background(t)
 
-    t = 20e-6
+    # pause for line trigger at 1ms
+    t = 0.001
+    add_time_marker(t, "Waiting for line trigger")
+    wait('line_trigger', t, timeout=0.1)
 
-    ct.set_background(t)
+    #turn off the MOT first
+    ct.Cs_Rep_Shutter__b1c12.go_low(t+0.01)
 
-    t = 30e-6
-
-    ct.set_background(t)
-
+    # Cs_MOT_Loading
     t = 0.1
-
     add_time_marker(t, 'Cs_MOT_Loading')
-    t = Cs_MOT_Loading(t+0.1, ct)
+    t = Cs_MOT_Loading(t, ct)
 
-    #close the repump shutter to get the background image
-    ct.Cs_Rep_Shutter__b1c12.go_low(t+5)
+    # wait 3 seconds for MOT to load
+    t+=3 
 
-    #reopen the repump shutter
-    ct.Cs_Rep_Shutter__b1c12.go_high(t+5.1)
+    # Cs_CMOT
+    add_time_marker(t, 'Cs_CMOT')
+    t = Cs_CMOT(t, ct)
 
-    ct.set_background(t+5.2)
+    # Cs_Molasses
+    add_time_marker(t, 'Cs_Molasses')
+    t = Cs_Molasses(t, ct)
 
-    stop(t+5.3)
+    # Cs_LF_H_Img
+    add_time_marker(t, 'Cs_LF_H_Imaging')
+    t = Cs_LF_H_Imaging(t, ct)
+
+    # set background values back
+    t += 0.001
+    ct.set_background(t)
+
+    # end sequence
+    t += 0.001
+    stop(t)
--- lics_labscript_apparatus/sequences/cs_subsequences.py
+++ lics_labscript_apparatus/sequences/cs_subsequences.py
@@ -5,223 +5,234 @@ if TYPE_CHECKING:
     from lics_labscript_apparatus._globals_stubs import *
 
 def Cs_MOT_Loading(t, ct):
-    """Function for loading the cesium MOT, starts at t-0.1 and ends at t+3.6 seconds"""
+    """Function for setting all values to load Cs MOT. 
+    Starts: t=0
+    Ends: t=0.005
+    Returns: t=0.005
+    Bitter coil must be in AH configuration BEFORE this function is called. 
+    Currently this subsequence doesn't touch the bitter coils at all. 
+    """
 
-    #set Cs laser frequencies
-    ct.Cs_MOT_Freq__b3c24.constant(t-0.1, -7.25)
 
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t, 2.3)
-    ct.Cs_Rep_Freq__b3c26.constant(t, 6.499939)
-
-    #close lithium shutters
+    # set Cs laser frequencies
+    ct.Cs_MOT_Freq__b3c24.constant(t, Cs_MOT_Freq_CsMOT)
+    ct.Cs_Rep_Freq__b3c26.constant(t, Cs_Rep_Freq_CsMOT)
+    ct.Cs_3DMOT_AO_AM__b3c21.constant(t, Cs_3DMOT_AO_AM_CsMOT)
+    
+    # close Li shutters
     ct.Li_Rep_Shutter__b2c01.go_low(t)
     ct.Li_MOT_Shutter__b1c31.go_low(t)
     ct.Li_Zeeman_Shutter__b2c03.go_low(t)
 
-    #bias field control
-    ct.Bias_X_plus__b3c03.constant(t, 0)
-    ct.Bias_X_minus__b3c04.constant(t, 0)
-    ct.Bias_Y_plus__b3c05.constant(t, 0)
-    ct.Bias_Y_minus__b3c06.constant(t, 0)
-    ct.Bias_Z_plus__b3c07.constant(t, 0)
-    ct.Bias_Z_minus__b3c08.constant(t, 0)
+    # open the Cs shutters
+    ct.Cs_Zeeman_Shutter__b1c17.go_high(t)
+    ct.Cs_2DMOT_Shutter__b1c01.go_high(t)
+    ct.Cs_3DMOT_AO_Sw__b1c02.go_high(t)
+    ct.Cs_3DMOT_Shutter__b1c03.go_high(t)
+    ct.Cs_Rep_Shutter__b1c12.go_high(t)
 
-    #bitter coil control
-    # ct.Bitter_Lower_FF__b3c14.constant(t, 0)
-    # ct.Bitter_HH_Upper_FF__b3c10.constant(t, 0)
-    # ct.Bitter_Upper_HH_Sw__b3c18.constant(t, 0)
+    # bias field control
+    ct.Bias_X_HH.constant(t, Bias_X_HH_CsMOT)
+    ct.Bias_X_AH.constant(t, Bias_X_AH_CsMOT)
+    ct.Bias_Y_HH.constant(t, Bias_Y_HH_CsMOT)
+    ct.Bias_Y_AH.constant(t, Bias_Y_AH_CsMOT)
+    ct.Bias_Z_HH.constant(t, Bias_Z_HH_CsMOT)
+    ct.Bias_Z_AH.constant(t, Bias_Z_AH_CsMOT)
 
-    # ct.Bitter_Lower_CC__b3c12.constant(t, 1)
-    # ct.Bitter_Lower_CV__b3c13.constant(t, 1.5)
-    # ct.Bitter_Upper_CC__b3c16.constant(t, 1)
-    # ct.Bitter_Upper_CV__b3c17.constant(t, 2)
-
-    #zeeman coil control
+    # zeeman coil control
     ct.Zeeman_C1__b4c10.constant(t, Zeeman_C1_CsMOT)
     ct.Zeeman_C2__b4c11.constant(t, Zeeman_C2_CsMOT)
     ct.Zeeman_C3__b4c12.constant(t, Zeeman_C3_CsMOT)
     ct.Zeeman_C4__b4c13.constant(t, Zeeman_C4_CsMOT)
     ct.Zeeman_C5__b4c14.constant(t, Zeeman_C5_CsMOT)
 
+    # bitter coil control
+    ct.Bitter_Lower_FF__b3c14.constant(t, 0)
+    ct.Bitter_HH_Upper_FF__b3c10.constant(t, 0)
+    ct.Bitter_Upper_HH_Sw__b3c18.constant(t, 0)
+    ct.Bitter_Lower_CC__b3c12.constant(t, 5)
+    ct.Bitter_Lower_CV__b3c13.constant(t, 2.3)
+    ct.Bitter_Upper_CC__b3c16.constant(t, 5)
+    ct.Bitter_Upper_CV__b3c17.constant(t, 2.4)
+    ct.Bitter_IServo_FB_Sw__b3c11.constant(t+0.002, 0)
+    ct.Bitter_Upper_AH_Sw__b3c15.constant(t+0.002, 5)
+    ct.Bitter_V_HH.constant(t+0.005, Bitter_V_HH_CsMOT)
+    ct.Bitter_V_AH.constant(t+0.005, Bitter_V_AH_CsMOT)
+
     #Dipole trap control
     #ct.oTOP_Mod_AM__b4c09.constant(t, 5)
     #ct.Dual_780_Int_Lock__b3c30.constant(t, 5)
     
-    #open the Cs 2D MOT and zeeman shutters
-    ct.Cs_Zeeman_Shutter__b1c17.go_high(t)
-    ct.Cs_2DMOT_Shutter__b1c01.go_high(t)
 
-    #open thre Cs 3D MOT shutter and switch on the 3D MOT AOM
-    ct.Cs_3DMOT_AO_Sw__b1c02.go_high(t+0.1)
-    ct.Cs_3DMOT_Shutter__b1c03.go_high(t+0.1)
-
-    #set bitter coil helmholtz/antihelmholtz configuration
-    # ct.Bitter_IServo_FB_Sw__b3c11.constant(t+0.002, 0)
-    # ct.Bitter_Upper_AH_Sw__b3c15.constant(t+0.002, 5)
-    # ct.Bitter_V_Lower__b3c19.constant(t+0.005, 0.17)
-    # ct.Bitter_V_Upper__b3c20.constant(t+0.005, -0.2)
-
-    return t+0.1
+    return t+0.005
 
 def Cs_CMOT(t, ct):
-    """function for preparing the cs compressed mot, starts at t-0.03 and ends at t+0.04898 """
+    """Function for compressing the Cs MOT. 
+    Starts: t=-0.030
+    Ends: t=0.049
+    Returns: t=0.050
+    This function assumes that the Cs MOT is already loaded and that 
+    the Bitter coils are in the AH configuration from the Cs_MOT_Loading 
+    function. The function will adjust the bias fields, MOT and repump 
+    frequencies and amplitudes, and the bitter coils to compress the MOT.
+    """
 
-    #aom control
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t-0.03, 2.300110)
+    # MOT AOM amplitude trajectory
+    ct.Cs_3DMOT_AO_AM__b3c21.ramp(t-0.030, 0.030, Cs_3DMOT_AO_AM_CsMOT, Cs_3DMOT_AO_AM_CsCMOT1, ct.FINE)
+    ct.Cs_3DMOT_AO_AM__b3c21.ramp(t+0.04, 0.008, Cs_3DMOT_AO_AM_CsCMOT1, Cs_3DMOT_AO_AM_CsCMOT2, ct.FINE)
 
-    #shutter control
-    ct.Cs_Zeeman_Shutter__b1c17.go_low(t-0.01)
-    ct.Cs_2DMOT_Shutter__b1c01.go_low(t-0.01)
+    # MOT frequency
+    ct.Cs_MOT_Freq__b3c24.ramp(t, 0.040, Cs_MOT_Freq_CsMOT, Cs_MOT_Freq_CsCMOT, ct.FINE)
 
-    #bias field control
-    ct.Bias_X_plus__b3c03.constant(t-0.01, -2.5)
-    ct.Bias_X_minus__b3c04.constant(t-0.01, 1.00061)
-    ct.Bias_Y_plus__b3c05.constant(t-0.01, -3.599854)
-    ct.Bias_Y_minus__b3c06.constant(t-0.01, 2.600098)
-    ct.Bias_Z_plus__b3c07.constant(t-0.01, -0.59976)
-    ct.Bias_Z_minus__b3c08.constant(t-0.01, -0.499878)
+    # set repump laser frequency
+    ct.Cs_Rep_Freq__b3c26.ramp(t+0.040, 0.009, Cs_Rep_Freq_CsMOT, Cs_Rep_Freq_CsCMOT, ct.FINE)
 
-    #turn on dipole trap intensity lock
-    #ct.Dual_780_Int_Lock__b3c30.constant(t-0.01, 5)
+    # turn off Zeeman slower and 2D MOT
+    ct.Cs_Zeeman_Shutter__b1c17.go_low(t-0.010)
+    ct.Cs_2DMOT_Shutter__b1c01.go_low(t-0.010)
 
-    #set mot and repump aom amplitudes
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t, 0.499878)
-    ct.Cs_Rep_AO_AM__b3c25.constant(t, 5)
+    # ramp bias fields 
+    ct.Bias_X_HH.ramp(t-0.010, 0.058, Bias_X_HH_CsMOT, Bias_X_HH_CsCMOT, ct.FINE)
+    ct.Bias_X_AH.ramp(t-0.010, 0.058, Bias_X_AH_CsMOT, Bias_X_AH_CsCMOT, ct.FINE)
+    ct.Bias_Y_HH.ramp(t-0.010, 0.058, Bias_Y_HH_CsMOT, Bias_Y_HH_CsCMOT, ct.FINE)
+    ct.Bias_Y_AH.ramp(t-0.010, 0.058, Bias_Y_AH_CsMOT, Bias_Y_AH_CsCMOT, ct.FINE)
+    ct.Bias_Z_HH.ramp(t-0.010, 0.058, Bias_Z_HH_CsMOT, Bias_Z_HH_CsCMOT, ct.FINE)
+    ct.Bias_Z_AH.ramp(t-0.010, 0.058, Bias_Z_AH_CsMOT, Bias_Z_AH_CsCMOT, ct.FINE)
 
     #imaging shutter control
-    ct.Cs_VRep_Shutter__b1c16.go_low(t)
+    ct.Cs_VOP_Shutter__b1c16.go_low(t)
     ct.Cs_HOP_Shutter__b1c09.go_low(t)
 
     #bitter coil control
-    ct.Bitter_V_Upper__b3c20.constant(t, -0.020142)
-    ct.Bitter_V_Lower__b3c19.constant(t, 0.188293)
-
-    #mot laser frequency control
-    ct.Cs_MOT_Freq__b3c24.constant(t, -7.200012)
+    ct.Bitter_V_HH.ramp(t, 0.040, Bitter_V_HH_CsMOT, Bitter_V_HH_CsCMOT1, ct.FINE)
+    ct.Bitter_V_HH.ramp(t+0.040, 0.009, Bitter_V_HH_CsCMOT1, Bitter_V_HH_CsCMOT2, ct.FINE)
+    ct.Bitter_V_AH.ramp(t, 0.040, Bitter_V_AH_CsMOT, Bitter_V_AH_CsCMOT1, ct.FINE)
+    ct.Bitter_V_AH.ramp(t+0.040, 0.009, Bitter_V_AH_CsCMOT1, Bitter_V_AH_CsCMOT2, ct.FINE)
 
     #turn off dipole trap intensity lock
     #ct.Dual_780_Int_Lock__b3c30.constant(t+0.04, 0)
 
-    #set mot aom amplitude
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t+0.04, 0.499878)
+    return t+0.050
 
-    #set repump laser frequency
-    ct.Cs_Rep_Freq__b3c26.constant(t+0.04, 6.499939)
-
-    #set bitter coils
-    ct.Bitter_V_Upper__b3c20.constant(t+0.04, -0.00916)
-    ct.Bitter_V_Lower__b3c19.constant(t+0.04, 0.112610)
-
-    #set mot laser frequency and amplitude
-    ct.Cs_MOT_Freq__b3c24.constant(t+0.04, -6.799927)
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t+0.048, 0.299988)
-
-    #set bias fields again
-    ct.Bias_X_plus__b3c03.constant(t+0.048, 2.000122)
-    ct.Bias_X_minus__b3c04.constant(t+0.048, 0)
-    ct.Bias_Y_plus__b3c05.constant(t+0.048, 0)
-    ct.Bias_Y_minus__b3c06.constant(t+0.048, -0.700073)
-    ct.Bias_Z_plus__b3c07.constant(t+0.048, 0.599976)
-    ct.Bias_Z_minus__b3c08.constant(t+0.048, -1.199951)
-
-    #set repump frequency again
-    ct.Cs_Rep_Freq__b3c26.constant(t+0.04898, 5.466919)
-
-    #set bitter coils again
-    ct.Bitter_V_Upper__b3c20.constant(t+0.04898, 0.010071)
-    ct.Bitter_V_Lower__b3c19.constant(t+0.04898, 0.075684)
-
-    return t+0.04898
-
-def Cs_Molasses_Cooling(t, ct):
-    """Function for doing Cs optical molasses cooling, starts at t-0.002 and ends at t+0.005"""
+def Cs_Molasses(t, ct):
+    """Function for doing Cs optical molasses cooling. Includes Cs_Molasses_Cooling 
+    and Cs_Molasses_Dark from the old sequence. 
+    Starts: t-0.002
+    Ends: t+0.005
+    Returns: t+0.005
+    """
     #adjust the MOT and repump frequencies
-    ct.Cs_MOT_Freq__b3c24.constant(t-0.002, -6.799927)
-    ct.Cs_Rep_Freq__b3c26.constant(t-0.001, 5.4666919)
+    ct.Cs_MOT_Freq__b3c24.ramp(t-0.002, 0.006, Cs_MOT_Freq_CsCMOT, Cs_MOT_Freq_Molasses, ct.FINE)
+    ct.Cs_Rep_Freq__b3c26.ramp(t-0.001, 0.002, Cs_Rep_Freq_CsCMOT, Cs_Rep_Freq_Molasses, ct.FINE)
 
-    #adjust the MOT and repump aom amplitudes
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t, 0.700073)
-    ct.Cs_Rep_AO_AM__b3c25.constant(t, 5)
+    #adjust the MOT and optical pump aom amplitudes
+    ct.Cs_3DMOT_AO_AM__b3c21.ramp(t, 0.005, Cs_3DMOT_AO_AM_CsCMOT2, Cs_3DMOT_AO_AM_Molasses, ct.FINE)
+    ct.Cs_OP_AO_AM__b3c25.constant(t, 5)
 
     #bitter coil control
-    ct.Bitter_Lower_FF__b3c14.constant(t, 0)
-    ct.Bitter_IServo_FB_Sw__b3c11.constant(t, 5)
-    ct.Bitter_Upper_AH_Sw__b3c15.constant(t, 0)
-    ct.Bitter_AH_Upper_FF__b3c09.constant(t, 0)
+    # ct.Bitter_Lower_FF__b3c14.constant(t, 0)
+    # ct.Bitter_IServo_FB_Sw__b3c11.constant(t, 5)
+    # ct.Bitter_Upper_AH_Sw__b3c15.constant(t, 0)
+    # ct.Bitter_AH_Upper_FF__b3c09.constant(t, 0)
 
     #bias coil control
-    ct.Bias_X_plus__b3c03.constant(t, 0.499878)
-    ct.Bias_X_minus__b3c04.constant(t, -1.00061)
-    ct.Bias_Y_plus__b3c05.constant(t, 2.864075)
-    ct.Bias_Y_minus__b3c06.constant(t, -1.499939)
-    ct.Bias_Z_plus__b3c07.constant(t, -1.00061)
-    ct.Bias_Z_minus__b3c08.constant(t, -0.400085)
+    # ct.Bias_X_plus__b3c03.constant(t, 0.499878)
+    # ct.Bias_X_minus__b3c04.constant(t, -1.00061)
+    # ct.Bias_Y_plus__b3c05.constant(t, 2.864075)
+    # ct.Bias_Y_minus__b3c06.constant(t, -1.499939)
+    # ct.Bias_Z_plus__b3c07.constant(t, -1.00061)
+    # ct.Bias_Z_minus__b3c08.constant(t, -0.400085)
 
-    #trigger the oscilloscope and the spectrum analyzer
-    ct.Spec_Analyzer_Trig__b2c09.go_high(t)
-    ct.Scope_Trig__b2c08.go_high(t)
-    ct.Spec_Analyzer_Trig__b2c09.go_low(t+0.001)
-    ct.Scope_Trig__b2c08.go_lowe(t+0.001)
+    # turn MOT light off after molasses
+    ct.Cs_3DMOT_AO_Sw__b1c02.go_low(t+0.005)
+    ct.Cs_3DMOT_Shutter__b1c03.go_low(t+0.005) # this was actually -7ms but I brought it up
 
-    #adjust the MOT and repump frequencies again
-    ct.Cs_Rep_Freq__b3c26.constant(t+0.001, 6.400146)
-    ct.Cs_MOT_Freq__b3c24.constant(t+0.004, -5.799866)
-
-    #adjust the MOT aom amplitude again
-    ct.Cs_3DMOT_AO_AM__b3c21.constant(t+0.005, 0.100098)
+    # reset MOT laser frequency after molasses
+    ct.Cs_MOT_Freq__b3c24.ramp(t+0.008, 0.001, Cs_MOT_Freq_Molasses, Cs_MOT_Freq_CsLFHImg, ct.FINE)
 
     return t+0.005
 
-def Cs_H_Imaging(t, ct):
-    """Function for doing cesium horizontal imaging, starts at t-0.413 and ends at t+0.655"""
-    ct.pco_panda.expose(t-0.413, name="absorption", frametype="im1", trigger_duration=0.1e-3)
-    ct.Pixelfly_Shutter__b2c06.go_high(t-15e-3)
+def Cs_LF_H_Imaging(t, ct):
+    """Function for doing cesium horizontal imaging at low field, i.e. after MOT.
+    Starts: t=-0.2 (background image with shutter closed) 
+    Ends: t+0.300
+    Returns: t+0.300
+    This function assumes that the bitter coil is OFF (AH and HH switch set to 0).
+    It also assumes that the Cs MOT light and such are turned off.
+    It's really designed to follow up the Cs_Molasses_Cooling function, which will 
+    have turned the coil off.
+    """
 
-    ct.Cs_HOP_AO_Sw__b1c08.go_low(t-13e-3)
-    ct.Cs_VRep_Shutter__b1c16.go_high(t-12e-3)
-    ct.Cs_HImg_Shutter__b1c07.go_high(t-10e-3)
-    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t-10e-3)
+    # collect initial background image with the shutter closed 
+    ct.pco_panda.expose(t-0.2, name="absorption", frametype="dark", trigger_duration=0.0005)
 
-    ct.Bias_Z_minus__b3c08.constant(t-10e-3, 0.499878)
-    ct.Bias_Y_minus__b3c06.constant(t-5e-3, -1.9950073)
+    # ATOM IMAGE at t=0.000
 
-    ct.Cs_VRep_Shutter__b1c16.go_low(t-1e-3)
+    # open the shutter well before the atoms image 
+    ct.Pixelfly_Shutter__b2c06.go_high(t-0.015)
 
-    ct.Cs_Rep_AO_AM__b3c25.constant(t-1e-3, 2.999878)
+    # prepare shutters and AOMS for atoms image
+    ct.Cs_OP_AO_Sw__b1c08.go_low(t-0.013)
+    ct.Cs_VOP_Shutter__b1c16.go_high(t-0.012)
 
-    ct.Cs_HOP_AO_Sw__b1c08.go_high(t-0.1e-3)
+    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t-0.013)
+    ct.Cs_HImg_Shutter__b1c07.go_high(t-0.012)
+    ct.Cs_LFImg_Shutter__b1c11.go_high(t-0.012)
+    
+    # set bias fields appropriately for imaging
+    # 
+    #
 
-    ct.pco_panda.expose(t-0.02e-3, name='absorption', frametype='im2', trigger_duration=0.08e-3)
 
+    # pulse the OP AOM right before/during the imaging pulse
+    ct.Cs_OP_AO_AM__b3c25.constant(t-0.001, 3)
+    ct.Cs_OP_AO_Sw__b1c08.go_high(t-0.001)
+    ct.Cs_OP_AO_Sw__b1c08.go_low(t+0.0001)
+    
+    # start atom image aquisition
+    ct.pco_panda.expose(t-0.003, name='absorption', frametype='atoms', trigger_duration=0.0005)
+
+    # pulse the imaging light
     ct.Cs_LFImg_AO_Sw__b1c10.go_high(t)
+    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.0001) # 100 us imaging pulse
+    
+
+    # close shutters and restore AOMs after image
+    ct.Cs_VOP_Shutter__b1c16.go_low(t-0.001)
+    ct.Cs_OP_AO_Sw__b1c08.go_high(t+0.010)
+
     ct.Cs_HImg_Shutter__b1c07.go_low(t)
-    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.6e-3)
-    ct.Cs_HOP_AO_Sw__b1c08.go_low(t+0.1e-3)
+    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+0.015)
 
-    ct.oTOP_Mod_AM__b4c09.constant(t+5e-3, 0)
-    ct.oTOP_AO_AM__b4c06.constant(t+5e-3, 0)
+    ct.Pixelfly_Shutter__b2c06.go_low(t+0.007)
 
-    ct.Pixelfly_Shutter__b2c06.go_low(t+7e-3)
+    # turn off dipole traps after image
+    # ct.oTOP_Mod_AM__b4c09.constant(t+5e-3, 0)
+    # ct.oTOP_AO_AM__b4c06.constant(t+5e-3, 0)
 
-    ct.Cs_HOP_AO_Sw__b1c08.go_high(t+10e-3)
-    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+15e-3)
+    # LIGHT IMAGE at t=0.200
+    ct.Pixelfly_Shutter__b2c06.go_high(t+0.185)
 
-    ct.Pixelfly_Shutter__b2c06.go_high(t+0.625)
+    # open shutter to prepare for image
+    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.187)
+    ct.Cs_HImg_Shutter__b1c07.go_high(t+0.188)
+    
+    # aquire image
+    ct.pco_panda.expose(t+0.196, name='absorption', frametype='light', trigger_duration=0.0005)
 
-    ct.Cs_HImg_Shutter__b1c07.go_high(t+0.625)
-    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.630)
+    # pulse imaging light
+    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+0.200)
+    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.2001) # 100 us imaging pulse
 
-    ct.pco_panda.expose(t+0.63998, name='absorption', frametype='im3', trigger_duration=0.08e-3)
+    # close shutters and restore AOMs after image
+    ct.Cs_HImg_Shutter__b1c07.go_low(t+0.200)
+    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+0.215)
 
-    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+0.640)
-    ct.Cs_HImg_Shutter__b1c07.go_low(t+0.640)
-    ct.Cs_LFImg_AO_Sw__b1c10.go_low(t+0.64006)
+    ct.Pixelfly_Shutter__b2c06.go_low(t+0.207)
 
-    ct.Pixelfly_Shutter__b2c06.go_low(t+0.647)
+    
 
-    ct.Cs_LFImg_AO_Sw__b1c10.go_high(t+0.655)
-
-    return t+0.655
+    return t+0.300
 
 if __name__ == '__main__':
     ct = ConnectionTable()
