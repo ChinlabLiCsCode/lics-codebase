a7b52447d31987f83b4a5fcefaa56ebe664c59ff
chinlablicsexp <chinlablics@gmail.com>
2026-08-07 17:40:43 -0500

    updated image analysis code, need to fizx issue with units

--- lics_labscript_apparatus/labscript_analysis/absorption_image_analysis.py
+++ lics_labscript_apparatus/labscript_analysis/absorption_image_analysis.py
@@ -24,35 +24,40 @@ ratio = np.divide(
         out = np.full(atoms_image.shape, 0.1, dtype=float),
         where = light_minus_dark != 0)
 ratio[ratio<=0]=0.1
-od_image = - np.log10(ratio)
-od_image[od_image==np.inf]=0
+
+log_image = - np.log10(ratio)
+log_image[log_image==np.inf]=0
+
+sigma0 = 3 * (852.34727582e-9) **2 / (2 * np.pi)
+
+od_image = log_image / sigma0
 
 fig, axes = plt.subplots(2,2, figsize=(10,10))
 
 extent=[0, 2048*6.5/1.2, 0, 2048*6.5/1.2]
 im1 = axes[0,0].imshow(dark_image, extent=extent)
-im1.set_title("dark_image")
+axes[0,0].set_title("dark_image")
 plt.colorbar(im1, ax=axes[0,0])
-im1.set_xlabel('x (um)')
-im1.ylabel('y (um)')
+axes[0,0].set_xlabel('x (um)')
+axes[0,0].set_ylabel('x (um)')
 
 im2 = axes[0,1].imshow(light_image, extent=extent)
 axes[0,1].set_title("light_image")
 plt.colorbar(im2, ax=axes[0,1])
-plt.xlabel('x (um)')
-plt.ylabel('y (um)')
+axes[0,1].set_xlabel('x (um)')
+axes[0,1].set_ylabel('x (um)')
 
 im3 = axes[1,0].imshow(atoms_image, extent=extent)
 axes[1,0].set_title("atoms_image")
 plt.colorbar(im3, ax=axes[1,0])
-plt.xlabel('x (um)')
-plt.ylabel('y (um)')
+axes[1,0].set_xlabel('x (um)')
+axes[1,0].set_ylabel('x (um)')
 
 im4 = axes[1,1].imshow(od_image, vmin=0, vmax=od_image.max(), extent=extent)
-axes[1,1].set_title("od_image")
+axes[1,1].set_title("2D Density")
 plt.colorbar(im4, ax=axes[1,1])
-plt.xlabel('x (um)')
-plt.ylabel('y (um)')
+axes[1,1].set_xlabel('x (um)')
+axes[1,1].set_ylabel('x (um)')
 
 fig.suptitle("Cs MOT Healthcheck:" + run_name)
 
@@ -60,4 +65,5 @@ run.save_result("Max Light Image Pixel Count", light_image.max())
 run.save_result("Max Atoms Image Pixel Count", atoms_image.max())
 run.save_result("Max OD Image Pixel Count", od_image.max())
 
+plt.tight_layout()
 plt.show()
